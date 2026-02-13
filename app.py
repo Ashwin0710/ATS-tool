eaddedcfd85d35d7a6263390ef0d0545d44c3244
Ashwin0710 <rithik3076@gmail.com>
2026-02-12 21:29:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 # Configure Google Gemini AI
 # Replace with your actual API key
-client = genai.Client(api_key="AIzaSyBP8HAPJt8ZtOVRdg0MHDNgmA4Jnj6EnnA")
+client = genai.Client(api_key="AIzaSyAOAmY5iWHc6HlOhmSdRgJcn_FChatDL3g")
 
 # Function to extract text from PDF
 def extract_text_from_pdf(pdf_path):
